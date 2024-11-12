b3d4a9d7abbba92674d7b9bca406484b16e886db
KesavCodes <kesavakumarcode@outlook.com>
2024-11-12 23:50:37 +0530

    [Adding google OAUTH endpoints in the backend]

--- dist/routes/auth/google.auth.route.js
+++ dist/routes/auth/google.auth.route.js
@@ -9,11 +9,18 @@ require("../../strategies/googleStrategy");
 const router = express_1.default.Router();
 router.get("/login", passport_1.default.authenticate("google"));
 router.get("/callback", passport_1.default.authenticate("google", {
-    successRedirect: "/",
-    failureRedirect: "/failure",
+    successRedirect: "/google/success",
+    failureRedirect: "/google/failure",
     session: true,
 }));
-router.get("/failure", (req, res) => {
+router.get("/google/success", (req, res) => {
+    var _a;
+    console.log(req.user, "-- From: req.user");
+    return res
+        .status(200)
+        .json({ message: "User Logged in successfully", user: (_a = req.user) === null || _a === void 0 ? void 0 : _a.id });
+});
+router.get("/google/failure", (req, res) => {
     return res.status(400).send("Login failed. Try again!");
 });
 exports.default = router;
--- src/routes/auth/google.auth.route.ts
+++ src/routes/auth/google.auth.route.ts
@@ -9,13 +9,20 @@ router.get("/login", passport.authenticate("google"));
 router.get(
   "/callback",
   passport.authenticate("google", {
-    successRedirect: "/",
-    failureRedirect: "/failure",
+    successRedirect: "/google/success",
+    failureRedirect: "/google/failure",
     session: true,
   })
 );
 
-router.get("/failure", (req: Request, res: Response) => {
+router.get("/google/success", (req: Request, res: Response) => {
+  console.log(req.user, "-- From: req.user");
+  return res
+    .status(200)
+    .json({ message: "User Logged in successfully", user: req.user?.id });
+});
+
+router.get("/google/failure", (req: Request, res: Response) => {
   return res.status(400).send("Login failed. Try again!");
 });
 
